7dcee379bba63d84311cdcea04d40e20985817ca
TristanRG <115103112+TristanRG@users.noreply.github.com>
2025-04-13 01:56:30 +0300

    Serializer updates to separate date and time

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import Transaction
+from datetime import datetime
 
 class TransactionSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
 
     class Meta:
         model = Transaction
-        fields = '__all__'
+        fields = ['id', 'user', 'description', 'amount', 'type', 'date', 'time']
         read_only_fields = ['id', 'user']
+
+    def get_date(self, obj):
+        return obj.created_at.strftime("%d %b %y")  
+
+    def get_time(self, obj):
+        return obj.created_at.strftime("%I.%M %p")
